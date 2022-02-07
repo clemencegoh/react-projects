538bf6e23d35e5042786ba14c080d200056ebe73
Clemence Goh <clemencegoh@gmail.com>
2022-02-07 17:28:21 +0800

    update paths

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import TicTacToePage from "pages/tic-tac-toe/TicTacToePage";
 import WordlePage from "pages/wordle/WordlePage";
 
+const baseUrl = process.env.PUBLIC_URL;
+const paths = {
+  home: `/${baseUrl}`,
+  tictactoe: `/${baseUrl}/games/tictactoe`,
+  wordle: `/${baseUrl}/games/wordle`,
+};
+
 function Home() {
   return (
     <div>
       <h1>Home</h1>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to={paths.home}>Home</Link>
         <br />
-        <Link to="/games/tictactoe">Tic tac toe</Link>
+        <Link to={paths.tictactoe}>Tic tac toe</Link>
         <br />
-        <Link to="/games/wordle">Wordle</Link>
+        <Link to={paths.wordle}>Wordle</Link>
       </nav>
     </div>
   );
@@ -23,9 +30,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/games/tictactoe" element={<TicTacToePage />} />
-        <Route path="/games/wordle" element={<WordlePage />} />
+        <Route path={paths.home} element={<Home />} />
+        <Route path={paths.tictactoe} element={<TicTacToePage />} />
+        <Route path={paths.wordle} element={<WordlePage />} />
       </Routes>
     </BrowserRouter>
   );
